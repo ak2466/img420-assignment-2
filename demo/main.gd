e8737b099416661994385325747497ad2d91c972
Alex King <ak2466@nau.edu>
2025-09-23 11:16:51 -0700

    Initial commit - tutorial followed

---
+++ demo/main.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func _on_Sprite2D_position_changed(node, new_pos):
+	print("The position of " + node.get_class() + " is now " + str(new_pos))
+
+
+func _on_gd_example_position_changed(node: Object, new_pos: Vector2) -> void:
+	print("The position of " + node.get_class() + " is now " + str(new_pos))
